ed746105d808844762fee93958258d915e37c694
amirih <hossein.amiri.enterprise@gmail.com>
2025-11-07 13:31:47 -0500

    change the variable name

--- code/utils/division.py
+++ code/utils/division.py
@@ -1,54 +1,56 @@
 import utils.string.fips as fips_utils
 
 
-def get_division_code(which="us"):
-    division = ()
+def get_division_codes(which="us"):
+    division_codes = ()
     length = 0
     if which == "d1":
-        d1_states = ["CT", "ME", "MA", "NH", "RI", "VT"]
-        division = tuple(fips_utils.get_state_fips_code(state) for state in d1_states)
+        division_1_states = ["CT", "ME", "MA", "NH", "RI", "VT"]
+        division_codes = tuple(fips_utils.get_state_fips_code(state) for state in division_1_states)
         length = 2
     elif which == "d2":
-        d2_states = ["NJ", "NY", "PA"]
-        division = tuple(fips_utils.get_state_fips_code(state) for state in d2_states)
+        division_2_states = ["NJ", "NY", "PA"]
+        division_codes = tuple(fips_utils.get_state_fips_code(state) for state in division_2_states)
         length = 2
     elif which == "d3":
-        d3_states = ["IL", "IN", "MI", "OH", "WI"]
-        division = tuple(fips_utils.get_state_fips_code(state) for state in d3_states)
+        division_3_states = ["IL", "IN", "MI", "OH", "WI"]
+        division_codes = tuple(fips_utils.get_state_fips_code(state) for state in division_3_states)
         length = 2
     elif which == "d4":
-        d4_states = ["IA", "KS", "MN", "MO", "NE", "ND", "SD"]
-        division = tuple(fips_utils.get_state_fips_code(state) for state in d4_states)
+        division_4_states = ["IA", "KS", "MN", "MO", "NE", "ND", "SD"]
+        division_codes = tuple(fips_utils.get_state_fips_code(state) for state in division_4_states)
         length = 2
     elif which == "d5":
-        d5_states = ["DE", "DC", "FL", "GA", "MD", "NC", "SC", "VA", "WV"]
-        division = tuple(fips_utils.get_state_fips_code(state) for state in d5_states)
+        division_5_states = ["DE", "DC", "FL", "GA", "MD", "NC", "SC", "VA", "WV"]
+        division_codes = tuple(fips_utils.get_state_fips_code(state) for state in division_5_states)
         length = 2
     elif which == "d6":
-        d6_states = ["AL", "KY", "MS", "TN"]
-        division = tuple(fips_utils.get_state_fips_code(state) for state in d6_states)
+        division_6_states = ["AL", "KY", "MS", "TN"]
+        division_codes = tuple(fips_utils.get_state_fips_code(state) for state in division_6_states)
         length = 2
     elif which == "d7":
-        d7_states = ["AR", "LA", "OK", "TX"]
-        division = tuple(fips_utils.get_state_fips_code(state) for state in d7_states)
+        division_7_states = ["AR", "LA", "OK", "TX"]
+        division_codes = tuple(fips_utils.get_state_fips_code(state) for state in division_7_states)
         length = 2
     elif which == "d8":
-        d8_states = ["AZ", "CO", "ID", "MT", "NV", "NM", "UT", "WY"]
-        division = tuple(fips_utils.get_state_fips_code(state) for state in d8_states)
+        division_8_states = ["AZ", "CO", "ID", "MT", "NV", "NM", "UT", "WY"]
+        division_codes = tuple(fips_utils.get_state_fips_code(state) for state in division_8_states)
         length = 2
     elif which == "d9":
-        d9_states = ["AK", "CA", "HI", "OR", "WA"]
-        division = tuple(fips_utils.get_state_fips_code(state) for state in d9_states)
+        division_9_states = ["AK", "CA", "HI", "OR", "WA"]
+        division_codes = tuple(fips_utils.get_state_fips_code(state) for state in division_9_states)
         length = 2
     elif which.upper() in fips_utils.get_state_fips_dict().keys():
-        division = tuple(fips_utils.get_state_fips_code(which))
+        division_codes = tuple(fips_utils.get_state_fips_code(which))
         length = 2
-    return division, length
+    return division_codes, length
 
 
 def get_filtered_df(input_df, division):
     print(f"Filtering len={len(input_df)} input_df for state codes: {division}")
-    division_code, length = get_division_code(which=division)
-    mask = input_df["poi_cbg"].astype("string").str[:length].isin(division_code)
+    division_codes, length = get_division_codes(which=division)
+    mask = input_df["poi_cbg"].astype("string").str[:length].isin(division_codes)
+    # TODO: Check this
+    # mask = input_df["poi_cbg"].astype("string").str.startswith(division_codes)
     input_df = input_df.loc[mask]
     return input_df
